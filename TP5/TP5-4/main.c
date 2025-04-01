547f185578e3c39139f67bcd516970d642bd203e
Bstiasss <53101755+Bstiasss@users.noreply.github.com>
2025-04-01 01:14:01 -0300

    update tp5-4

--- TP5/TP5-4/main.c
+++ TP5/TP5-4/main.c
@@ -6,6 +6,7 @@ int main()
 {
     char cadena[50];
     char copia[50];
+    char segundaCadena[50];
 
     printf("Ingrese una cadena de texto: ");
     fflush(stdin);
@@ -14,5 +15,14 @@ int main()
     strcpy(copia, cadena);
 
     printf("Las cadenas son: \nOriginal: %s \nCopia: %s", cadena, copia);
+
+    printf("\nIngrese una segunda cadena: ");
+    fflush(stdin);
+    gets(segundaCadena);
+
+    strcat(copia, segundaCadena);
+
+    printf("La cadena resultante es: %s", copia);
+
     return 0;
 }
